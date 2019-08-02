2988af293f37dfc5fd3a626459dd478469d241e5
John Azzaro <jazzaro@temple.edu>
2019-08-02 09:20:16 -0700

    updated server.js, pn additions

--- server.js
+++ server.js
@@ -10,38 +10,38 @@ const jsonParser = bodyParser.json();
 const app = express();
 
 
-app.use(morgan('common'));                                                                     // log the http layer
+app.use(morgan('common'));                                                                   
 
 
-ShoppingList.create('beans', 2);                                                               // add some items to ShoppingList so there's some data to look at
+ShoppingList.create('beans', 2);                                                              
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
 
 Recipes.create(
-  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);                 // adding some recipes to `Recipes` so there's something to retrieve.
+  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);                 
 Recipes.create(
   'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
 
 
-app.get('/shopping-list', (req, res) => {                                                      // when the root of this router is called with GET, return all current ShoppingList items
+app.get('/shopping-list', (req, res) => {                                                      
   res.json(ShoppingList.get());
 });
 
-//POST//                                                                                       
-app.post('/shopping-list', jsonParser, (req, res) => {
-  const requiredFields = ['name', 'budget'];                                                    // ensure `name` and `budget` are in request body
-  for (let i=0; i<requiredFields.length; i++) {
+//POST//                                                                                      
+app.post('/shopping-list', jsonParser, (req, res) => {                                          // When the client sends a POST request to the endpoint /shopping-list (also passing json parser as a second argument with request response objects)
+  const requiredFields = ['name', 'budget'];                                                    // first you need to VALIDATE "name" and "budget" and make sure they are in request body by creating a constant with an array with "name" and "budget" in it....
+  for (let i=0; i<requiredFields.length; i++) {                                                 // Now as you loop through the the list for name and budget....
     const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
+    if (!(field in req.body)) {                                                                 // if the field is NOT in the request body (i.e. name and budget are NOT in the request body)...
+      const message = `Missing \`${field}\` in request body`                                    // send an error message
       console.error(message);
       return res.status(400).send(message);
     }
   }
 
-  const item = ShoppingList.create(req.body.name, req.body.budget);
-  res.status(201).json(item);
+  const item = ShoppingList.create(req.body.name, req.body.budget);                              // But if the request sent is VALID, then the item will be sent, which would be name and the body via the Shoppinglist.create method.
+  res.status(201).json(item);                                                                    // And the response will be the new item, a 201 status (good), and the json object!
 });
 
 
